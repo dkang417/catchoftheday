7a630e714a0251c94a9233838a54e5bdb2e08ace
dkang417 <dkang417@gmail.com>
2019-09-01 21:22:52 -0400

    first commit

---
+++ src/components/StorePicker.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+    render() {
+        return (
+            <form className="store-selector" >
+                <h2>please entere a store</h2>
+                <input type="text" required placeholder="store Name" />
+                <button type="submit">Visit store -> </button>
+            </form>
+        )
+    }
+}
+
+export default StorePicker;
